c0e66d5399de8cd839cc01d0b27cfd8b8c69b9ea
GFTER <47288139+GFERC@users.noreply.github.com>
2019-09-04 22:50:40 -0700

    Update mvS3Objects.sh

--- aws/s3/scripts/mvS3Objects.sh
+++ aws/s3/scripts/mvS3Objects.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# this script is to copy s3 objects from one bucket in one aws account to another bucket in another aws account
+# input: fileName.txt contains s3 object names that are needed to copy to 
 # chmod 755 mvS3Objects.sh to make sure you can execute this file
 # how to run ./mvS3Objects.sh fileName.txt
+
 FILE=$1
 while read LINE; do
      aws s3 sync s3://source-0904-test s3://desti-0904-test --exclude "*" --include "$LINE" --profile fightingProfile
